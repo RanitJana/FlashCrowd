f13f837cd9d79ffd1531012f85ead15d53730ae7
Ranajit2005 <18ranajit2005@gmail.com>
2025-06-21 00:22:35 +0530

    ui update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import Map from "./components/Map"
-
+import Event from "./components/Event"
 
 
 export default function App() {
 
   return (
+    <>
     <Map />
+    <Event/>
+    </>
   )
 
 }
---
+++ client/src/components/Event.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Event = () => {
+  return (
+    <div>
+      Event show section
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis at minima et dolore ipsa nesciunt minus dicta inventore, voluptate, quia ullam. Molestiae blanditiis nemo ad nesciunt. Ab repudiandae quas quod, nesciunt asperiores impedit quaerat quis enim expedita necessitatibus et hic, aliquam debitis officia repellat iure vero voluptas id sequi facilis magni natus? Blanditiis atque iste aut quo enim. Aut quae ipsam ullam. Ea nobis necessitatibus veniam iste exercitationem ullam maiores natus impedit sint maxime beatae, quasi nam odit dolor iure dolorum odio aspernatur modi magnam inventore. Omnis unde ut ullam obcaecati, aliquid cum rerum eligendi laboriosam quia repudiandae quas aspernatur.
+    </div>
+  )
+}
+
+export default Event
