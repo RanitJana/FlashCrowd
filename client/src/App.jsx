cb5427aee4cf35aca1f8358acdc96e950d2e4a7d
Ranajit2005 <18ranajit2005@gmail.com>
2025-06-21 11:08:49 +0530

    feat: add route for AddEvent page in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import AddEvent from "./pages/AddEvent.jsx";
 import Home from "./pages/Home.jsx";
 import Profile from "./pages/Profile.jsx";
 import { Route, Routes } from "react-router";
@@ -7,6 +8,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/addEvent" element={<AddEvent/>} />
     </Routes>
   );
 }
