398f0eb67e762a4f7346bc637a9ed49f31d8fe16
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2025-06-21 16:50:18 +0530

    resolve

--- server/src/routes/event.route.js
+++ server/src/routes/event.route.js
@@ -1,19 +1,19 @@
 import express from "express";
 const router = express.Router();
 import {
-  addParticipant,
-  createEvent,
-  getHostedEvents,
-  getOngoingEvents,
-  getParticipatedEvents,
-  getUpcomingEvents,
+    addParticipant,
+    createEvent,
+    getHostedEvents,
+    getOngoingEvents,
+    getParticipatedEvents,
+    getUpcomingEvents,
 } from "../controllers/event.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 router.route("/").post(verifyJWT, createEvent);
 router.route("/hosted").get(verifyJWT, getHostedEvents);
 router.route("/participated").get(verifyJWT, getParticipatedEvents);
-router.route("/add-participant").post(verifyJWT, addParticipant);
+router.route("/add-participant").put(verifyJWT, addParticipant);
 router.route("/currEvent").get(getOngoingEvents);
 router.route("/upcomingEvent").get(getUpcomingEvents);
 
