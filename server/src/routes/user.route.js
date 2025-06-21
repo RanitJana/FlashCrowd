24ee6d3005883e137bdf1de9dd220fb320b15c0a
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2025-06-21 15:03:19 +0530

    feat friend controller and router

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 import { handleLogin, handleLogout } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
-  searchFriends,
   searchUsers,
   updateUserInfo,
 } from "../controllers/user.controller.js";
@@ -15,6 +14,5 @@ router
   .route("/update")
   .post(verifyJWT, upload.single("profilePicture"), updateUserInfo);
 router.route("/search").get(verifyJWT, searchUsers);
-router.route("/search/friends").get(verifyJWT, searchFriends);
 
 export default router;
