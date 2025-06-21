ca738144581e8a3f2c25603364c8987a25577a50
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2025-06-21 11:25:59 +0530

    feat search user,friend,update info

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
 import { handleLogin, handleLogout } from "../controllers/auth.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { searchFriends, searchUsers, updateUserInfo } from "../controllers/user.controller.js";
 
 router.route("/login").post(handleLogin);
 router.route("/logout").post(handleLogout);
+router.route("/update").post(verifyJWT, updateUserInfo);
+router.route("/search").get(verifyJWT, searchUsers);
+router.route("/search/friends").get(verifyJWT, searchFriends);
 
 export default router;
