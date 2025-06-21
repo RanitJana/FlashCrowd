85cb9ebb537be3337c4317de4d515fbdbde966c3
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2025-06-21 10:03:12 +0530

    feat get HostedEvent, ParticipatedEvent,addParticipant, ongoingEvents

--- server/src/controllers/event.controller.js
+++ server/src/controllers/event.controller.js
@@ -1,4 +1,5 @@
 import eventSchema from "../models/event.model.js";
+import ParticipationSchema from "../models/participation.model.js";
 import AsyncHandler from "../utils/AsyncHandler.js";
 
 const createEvent = AsyncHandler(async (req, res) => {
@@ -43,4 +44,106 @@ const createEvent = AsyncHandler(async (req, res) => {
   });
 });
 
-export { createEvent };
+const getHostedEvents = AsyncHandler(async (req, res) => {
+  const events = await eventSchema
+    .find({ host: req.user._id })
+    .populate("participants", "-password -refreshToken");
+
+  if (!events || events.length === 0) {
+    return res.status(404).json({
+      success: false,
+      message: "No hosted events found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Hosted events retrieved successfully",
+    events,
+  });
+});
+
+const getParticipatedEvents = AsyncHandler(async (req, res) => {
+  const participation = await ParticipationSchema.findOne({
+    user: req.user._id,
+  })
+    .populate("events.id", "-participants")
+    .select("events");
+
+  if (
+    !participation ||
+    !participation.events ||
+    participation.events.length === 0
+  ) {
+    return res.status(404).json({
+      success: false,
+      message: "No participated events found",
+    });
+  }
+
+  const events = participation.events.map((e) => e.id);
+  return res.status(200).json({
+    success: true,
+    message: "Participated events retrieved successfully",
+    events,
+  });
+});
+
+const addParticipant = AsyncHandler(async (req, res) => {
+  const { eventId, participantId } = req.body;
+  if (!eventId || !participantId) {
+    return res.status(400).json({
+      success: false,
+      message: "Event ID and Participant ID are required",
+    });
+  }
+  const event = await eventSchema.findById(eventId);
+  if (!event) {
+    return res.status(404).json({
+      success: false,
+      message: "Event not found",
+    });
+  }
+  if (event.participants.includes(participantId)) {
+    return res.status(400).json({
+      success: false,
+      message: "Participant already added to the event",
+    });
+  }
+  if (event.participants.length >= event.maxLimit) {
+    return res.status(400).json({
+      success: false,
+      message: "Event is full, cannot add more participants",
+    });
+  }
+  event.participants.push(participantId);
+  await event.save();
+
+  return res.status(200).json({
+    success: true,
+    message: "Participant added successfully",
+    event,
+  });
+});
+
+const getOngoingEvents = AsyncHandler(async (req, res) => {
+  const now = new Date();
+
+  const events = await Event.find({
+    startTime: { $lte: now },
+    endTime: { $gte: now },
+  }).populate("host", "name email");
+
+  res.status(200).json({
+    success: true,
+    events,
+  });
+});
+
+export {
+  createEvent,
+  getHostedEvents,
+  getParticipatedEvents,
+  addParticipant,
+  getOngoingEvents,
+};
